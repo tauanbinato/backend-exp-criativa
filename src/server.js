506f47e2c2f2d7dd7d08351ef36b2c73b1d3c1ab
Tauan Binato <tauanflores@gmail.com>
2021-10-26 16:33:13 -0400

    adding cors

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 require('./database');
 
@@ -9,5 +10,5 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
-
+app.use(cors)
 app.listen(process.env.PORT || 3333);
