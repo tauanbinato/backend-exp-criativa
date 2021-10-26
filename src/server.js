a142863ad48d84b20f68987f7914e39b8f8dc7b8
Tauan Binato <tauanflores@gmail.com>
2021-10-26 17:44:51 -0400

    minor 2

--- src/server.js
+++ src/server.js
@@ -9,11 +9,12 @@ require('./database');
 const app = express();
 
 app.use(express.json());
-app.use((req,res, next) => {
-    //console.log('acessou mid');
-    res.header("Access-Control-Allow-Origin", "*");
+app.use((req,res,next) => {
+    console.log('q');
+    req.header("Access-Control-Allow-Origin", "*");
     app.use(cors);
     next();
+
 });
 app.use(routes);
 app.listen(process.env.PORT || 3333);
