0a38bd4e3131431bd57c2904a8a1755461a5c8f7
Tauan Binato <tauanflores@gmail.com>
2021-10-27 15:24:57 -0400

    allowing requests from localhost and prod only

--- src/server.js
+++ src/server.js
@@ -10,7 +10,12 @@ const app = express();
 
 app.use(express.json());
 app.use((req,res,next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:8080', 'https://esuper.adazulpool.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,access-control-allow-origin");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     app.use(cors);
