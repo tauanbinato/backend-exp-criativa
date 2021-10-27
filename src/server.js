eb00780a36d1a5e4222b000db6a239e673b9de71
Tauan Binato <tauanflores@gmail.com>
2021-10-27 14:51:27 -0400

    allowing cors

--- src/server.js
+++ src/server.js
@@ -11,10 +11,11 @@ const app = express();
 app.use(express.json());
 app.use((req,res,next) => {
     console.log('q');
-    req.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,access-control-allow-origin");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     app.use(cors);
     next();
-
 });
 app.use(routes);
 app.listen(process.env.PORT || 3333);
